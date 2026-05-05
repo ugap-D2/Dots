a8c19704ca54dd10f84fa7ec3df861926db876eb
ugap-D2 <ugap.jordan@gmail.com>
2026-05-05 20:39:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 # copy kitty.conf and the kitty theme to kitty directory
 cp kitty.conf ~/.config/kitty
 cp rhel-theme.conf ~/.config/kitty
+mkdir -p ~/Pictures/logos
+cp logo/RH-logo.png ~/Pictures/logos
 
 # make the directory needed to add micro colorschemes then copy the theme into it
 mkdir -p  ~/.config/micro/colorschemes
-cp micro-conf ~/.config/micro/colorschemes
+cp rhel-theme.micro ~/.config/micro/colorschemes
 
-mv bashrc .bashrc
-cp .bashrc ~/.
+cp .bashrc ~/
 
-echo "v micro editorju dj ctrl+e pa vpiÅ¡i set colorscheme rhel-theme"
+echo "to use the micro colorscheme you will need to run micro then do ctrl+e and type set colorscheme rhel-theme"
